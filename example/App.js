5467fc60f2ddace5753aa7212665d8ab643a953b
Roman Rädle <raedle@fb.com>
2021-07-18 17:06:19 -0700

    chore: working jsi

--- example/App.js
+++ example/App.js
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+function argmax(arr) {
+  let maxValue = -Number.MAX_VALUE;
+  let maxIdx = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+      maxIdx = i;
+    }
+  }
+  return maxIdx;
+}
+
 export default function App() {
   const [result, setResult] = React.useState();
 
   React.useEffect(() => {
-    setResult(global.helloWorld())
+    let maxIdx = -1;
+    let totalTime = 0;
+    let iterations = 10000;
+    for (let i = 0; i < iterations; i++) {
+      const startTime = Date.now();
+      maxIdx = argmax(global.myArr);
+      totalTime += Date.now() - startTime;
+    }
+    console.log({
+      time: totalTime / iterations
+    })
+    setResult(maxIdx);
   }, []);
 
   return (
